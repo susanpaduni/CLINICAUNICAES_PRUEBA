d33e8d0f57fb141789ccdd37cd589af8f34df5da
Susan <susan.padilla@catolica.edu.sv>
2026-07-14 20:57:13 -0600

    Menu principal del sistema

--- CLINICAUNICAES_PRUEBA/Program.cs
+++ CLINICAUNICAES_PRUEBA/Program.cs
@@ -1,1 +1,64 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class ClinicaUnicaes 
+{
+    static void Main(string[] args)
+    {
+        MenuPrincipal();
+    }
+
+    static void MenuPrincipal()
+    {
+        int opcion = 0;
+        do
+        {
+            Console.WriteLine("============================");
+            Console.WriteLine("CLINICA MEDICA UNICAES");
+            Console.WriteLine("Sistema Administrativo");
+            Console.WriteLine("============================");
+            Console.WriteLine("1.Adminitracion de Pacientes");
+            Console.WriteLine("2.Administracion de Medicos");
+            Console.WriteLine("3.Administracion de Consultorios");
+            Console.WriteLine("4.Administracion de Citas");
+            Console.WriteLine("5.Administracion de Especialidades");
+            Console.WriteLine("6.Salir");
+            Console.WriteLine();
+            Console.WriteLine("Seleccione una opcion:");
+
+            if (int.Parse(Console.ReadLine(), out opcion))
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        MenuPacientes();
+                        break;
+                    case 2:
+                        MenuMedicos();
+                        break;
+                    case 3:
+                        MenuConsultorios();
+                        break;
+                    case 4:
+                        MenuCitas();
+                        break;
+                    case 5:
+                        MenuEspecialidades();
+                        break;
+                    case 6:
+                        Console.WriteLine("Saliendo del sistema...");
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida. Presione Enter para intentar de nuevamente.");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ingrese un numero valido.Presione Enter");
+                Console.ReadLine();
+                opcion = 0;
+
+                
+            }
+        } while (opcion != 6);
+    }
+}
